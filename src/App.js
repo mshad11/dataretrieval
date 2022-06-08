95ae8a8af9db8e08e501325248a47e0787ac8bdf
Shahid <malikshahid886@gmail.com>
2022-06-08 11:57:20 +0530

    data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React, {useState , useEffect} from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const[data,setData] = useState([]);
+useEffect(() => {fetch('https://jsonplaceholder.typicode.com/users') // READ =>GET request to retrieve record
+.then((response) => response.json())
+.then((result) => setData(result))
+.catch((err) =>err)
+console.log(data)
+},[])
+  useEffect(() => {
+    //POST request by using fetch method
+    const requestOptions = {
+      method: "POST",
+      headers: {"Content-Type" : "application/json"},
+      body: JSON.stringify ({username: "James" , email: "james@gmail.com", id: "100" })
+      
+    };
+    fetch('https://jsonplaceholder.typicode.com/users',requestOptions)
+    .then((response) => response.json())
+    .then((data) => console.log(data)) 
+  } , [])
+  + (
+     < div className="app">
+     
+     <h1>API</h1>
+     {data.map((item) => (<div className='box'>
+       
+       <p> name:{item.name}</p>
+       <p>username: {item.username}</p> 
+       <p>email: {item.email}</p>
+       <p>phone: {item.phone}</p>
+       </div>))}
+     
+     
+     </div>
   );
 }
 
